964098ec08b54c12f7ce15048778580fe3f91cf1
TheDalpha <oliv9858@easv365.dk>
2018-05-14 11:36:35 +0200

    ConfigView UI Implementation

--- Shoreline/src/shoreline/gui/controller/ConfigureViewController.java
+++ Shoreline/src/shoreline/gui/controller/ConfigureViewController.java
@@ -5,9 +5,20 @@
  */
 package shoreline.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -17,6 +28,21 @@ import javafx.fxml.Initializable;
 public class ConfigureViewController implements Initializable
 {
 
+    @FXML
+    private Label lblUser;
+    @FXML
+    private JFXComboBox<String> savedCombo;
+    @FXML
+    private ListView<String> selectedList;
+    @FXML
+    private JFXButton saveConfBtn;
+    @FXML
+    private JFXButton addAttributeBtn;
+    @FXML
+    private ListView<?> attributeView;
+    @FXML
+    private JFXButton cancelBtn;
+
     /**
      * Initializes the controller class.
      */
@@ -25,5 +51,30 @@ public class ConfigureViewController implements Initializable
     {
         // TODO
     }    
+
+
+    @FXML
+    private void saveConfiguration(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void addAtribute(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void cancel(ActionEvent event) throws IOException
+    {
+        Stage stage1 = (Stage) cancelBtn.getScene().getWindow();
+        stage1.close();
+
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/shoreline/gui/view/UserView.fxml"));
+        root.getStylesheets().add("/shoreline/gui/view/Css/Style.css");
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
